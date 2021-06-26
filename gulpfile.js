8bba24baa068d668c6d7be13c422681d4347ae75
Anish Tipnis <rusty3699@gmail.com>
2021-06-26 18:52:52 +0530

    updated dist/assets
    pipeline set, new folder copied along with clean

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 var concat = require('gulp-concat');
+var del = require('del');
+
 //source html
 var html_src='./index.html'
 //dest html
@@ -30,3 +32,25 @@ gulp.task('styles', function() {
     .pipe(htmlmin({minifyCSS: true}))
     .pipe(gulp.dest(css_dist));
 });
+gulp.task('clean', function(){
+  return del('dist/**', {force:true});
+});
+
+//images
+gulp.task('copyimg', function () {
+  return  gulp.src('./assets/images/**')
+        .pipe(gulp.dest('dist/assets/images/'));
+});
+
+//resume
+gulp.task('copyres', function () {
+  return  gulp.src('./assets/resume/**')
+        .pipe(gulp.dest('dist/assets/resume/'));
+});
+
+
+
+
+
+//npm run build
+gulp.task('build',gulp.series('clean','copyimg','copyres','minify-html','styles'));
