d1a2dd1f8ec868a0ec0e451c54fe0fa534850911
Anish Tipnis <rusty3699@gmail.com>
2021-06-26 12:56:18 +0530

    Gulp
    dist folder is being made

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,31 @@
-/*
+
+//to run gulp minify-html
+//to run gulp styles
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
-
-
-var html_file='./index4.html'
-
-var html_dist='./index3.html'
-
-gulp.task('minify-html', function() {
-  return gulp.src(html_file)
-          .pipe(htmlmin())
-          .pipe(gulp.dest(html_dist));
-});
-*/
-var gulp = require('gulp');
-var htmlmin = require('gulp-htmlmin');
-
-//source
+var concat = require('gulp-concat');
+//source html
 var html_src='./index.html'
-//dest
-var html_dist='./index5.html'
+//dest html
+var html_dist='dist'
+//css source
+//var css_src='./assets/css'
+var css_dist='dist/css'
 
 
-//minify
+//minify html and js
 gulp.task('minify-html', function() {
 return gulp.src(html_src)
-.pipe(htmlmin())
+    .pipe(htmlmin({collapseWhitespace:true}))
+    .pipe(htmlmin({removeComments:true}))
+    .pipe(htmlmin({minifyJS: true}))
+
 .pipe(gulp.dest(html_dist));
+});
+
+//concat the css filess 
+gulp.task('styles', function() {
+  gulp.src("./assets/css/**.css")
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest(css_dist));
 });
